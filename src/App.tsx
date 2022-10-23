ab913294396bbdf8a520b7008199094f68125a3d
todojinu <nukukkoya@gmail.com>
2022-10-23 18:54:58 +0900

    1-4. event 함수로 전달받는 event의 타입 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello ", value);
+  };
+
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          type="text"
+          placeholder="username"
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
