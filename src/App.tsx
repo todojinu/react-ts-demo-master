db4841e9520ab5386a8caeaad90865a76c5c95df
todojinu <nukukkoya@gmail.com>
2022-10-23 19:35:10 +0900

    1-5. declarations file 을 이용한 theme 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.div`
+  colors: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello ", value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          type="text"
-          placeholder="username"
-          onChange={onChange}
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>proptected</H1>
+    </Container>
   );
 }
 
