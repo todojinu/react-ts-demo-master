cf8464f9ef07df73c933d7b311e849ff3dc26c05
todojinu <nukukkoya@gmail.com>
2022-10-25 23:59:26 +0900

    2-1. Crypto Tracker: react-router-dom, react-query 설치 및 Router 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.div`
-  colors: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
-  return (
-    <Container>
-      <H1>proptected</H1>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
